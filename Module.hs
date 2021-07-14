8dc671338b0cc40fbd7f512ffbd23e0fc7b6c9ee
Paul Horikx <phorikx@sogyo.nl>
2021-07-14 08:50:50 +0200

    added triangleNumber function

--- Module.hs
+++ Module.hs
@@ -6,11 +6,13 @@ n !^! k = fac n / (fac k * fac (n - k))
 kwadraat x = x*x
 somVanKwadraten lijst = sum( map kwadraat lijst)
 -- Functions for the triangle number
+triangleNumber:: Int -> Int
 triangleNumber n = sum [1..n]
+doesDivide:: Int -> Int -> Bool
 doesDivide n k  = ((mod n k) == 0)
 dividors :: Int -> [Int]
 dividors k = filter (doesDivide k) [1..z] ++ [k] where z = k `div` 2
 getTriangleNumber:: Int -> Int -> Int
 getTriangleNumber k n
-    | length(dividors(triangleNumber n)) <= k      = getTriangleNumber k (n+1)
-    | otherwise                                         = triangleNumber n
+    | k > (length.dividors.triangleNumber) n      = getTriangleNumber k (n+1)
+    | otherwise                                   = triangleNumber n
